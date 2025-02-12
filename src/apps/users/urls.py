45ec0fc5eb7a021bbec332457b599a437b72c6ef
Bahati <139157541+Iambahati@users.noreply.github.com>
2025-02-12 17:49:26 +0300

    feat(users): add signup and signin views, update URLs and navbar links

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import (index)
+from .views import (index, signup_view, signin_view,
+                    # signout_view, dashboard_view, profile_view, profile_update_view, password_change_view, password_change_done_view, password_reset_view, password_reset_done_view, password_reset_confirm_view, password_reset_complete_view,)
+                    
+                    )
 
 urlpatterns = [
     path("", index, name="index"),
+    path("register/", signup_view, name="register"), # User registration
+    path("login/", signin_view, name="login"), # User login  
 ]
--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'pages/index.html')
+
+def signup_view(request):
+    return render(request, 'pages/auth/signup.html')
+
+
+def signin_view(request):
+    return render(request, 'pages/auth/signin.html')
