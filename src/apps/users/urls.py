ed3edfb94ebb370774a9dddef9b716db59ec94ac
Bahati <139157541+Iambahati@users.noreply.github.com>
2025-03-05 10:39:26 +0300

    feat(users): update URL patterns to include features and testimonials pages

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (index, signup_view, signin_view, about, contact
+from .views import (index, signup_view, signin_view, features, testimonials
                     # signout_view, dashboard_view, profile_view, profile_update_view, password_change_view, password_change_done_view, password_reset_view, password_reset_done_view, password_reset_confirm_view, password_reset_complete_view,)
                     
                     )
@@ -8,6 +8,6 @@ urlpatterns = [
     path("", index, name="index"),
     path("register/", signup_view, name="register"), # User registration
     path("login/", signin_view, name="login"), # User login  
-    path("about/", about, name="about"),
-    path("contact/", contact, name="contact"),
+    path("features/", features, name="features"),
+    path("testimonials/", testimonials, name="testimonials"),
 ]
